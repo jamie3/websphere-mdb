88d6b326c20419a4a48c0e66e983b028257abb85
Jamie Archibald <jarchibald@westjet.com>
2015-04-06 13:45:00 -0600

    Changed project structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# Message Driven Bean on WebSphere v8.0
+
+Simple example showing how to make a message driven bean on websphere
+
+# Author
+
+jamie3@gmail.com
---
+++ websphere-mdb-ejb/ejbModule/example/ejb/MyMDB.java
@@ -0,0 +1,46 @@
+package example.ejb;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+import example.jar.Echo;
+
+/**
+ * Session Bean implementation class MySessionBean
+ */
+@MessageDriven(activationConfig = { @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") }, mappedName = "jms/ENSQueue")
+public class MyMDB implements javax.jms.MessageListener {
+
+	Echo echo;
+	
+    /**
+     * Default constructor. 
+     */
+    public MyMDB() {
+        // TODO Auto-generated constructor stub
+    	
+    }
+
+    @PostConstruct
+    public void init() {
+    	System.out.println("initializing ejb");
+    	echo = new Echo();
+    }
+
+	@Override
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		TextMessage text = (TextMessage)message;
+    	try {
+    		String msg = text.getText();
+			echo.echo("Message from JMS: " + text.getText());
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
